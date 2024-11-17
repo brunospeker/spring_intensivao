78b17a785d9ea3007ed3fa974f85af708b526401
Bruno Speker <bruno.speker@gmail.com>
2024-11-17 20:57:24 -0300

    project created

---
+++ src/main/java/com/devsuperior/intensivao/IntensivaoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.intensivao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensivaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensivaoApplication.class, args);
+	}
+
+}
